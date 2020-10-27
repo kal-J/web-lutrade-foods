cc6c8cbf13380b2f9206e59a13102500f003af8a
kal-J <kharljay15@gmail.com>
2020-10-27 09:09:08 +0300

    fix: remove warnings

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Box, Button, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import colors from "./layout/colors";
 const useStyles = makeStyles((theme) => ({
   container: {
     display: "flex",
